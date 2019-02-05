7ca70d5e0827ddb9d8bd257b4b5c161e0481ce62
Brie <723547+brieb@users.noreply.github.com>
2019-02-04 16:22:38 -0800

    Create types/your-package as an example

---
+++ types/your-package/index.d.ts
@@ -0,0 +1,7 @@
+// Internal type definitions for @your-internal-scope/your-package 1.0.0
+// Project: https://your.internal.git.url/your-package
+// Definitions by: Your User <https://your.internal.git.url/username>
+// Definitions: https://your.internal.git.url/types
+// TypeScript Version: 2.9
+
+export default function greeter(name: string): string;
